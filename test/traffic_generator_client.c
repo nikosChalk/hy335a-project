42d21785f6a93945be761e4b5788cbf5a47b080a
unknown <isimantiraki@gmail.com>
2017-11-26 14:31:58 +0200

    traffic_generator_client.c done!

--- test/traffic_generator_client.c
+++ test/traffic_generator_client.c
@@ -29,6 +29,8 @@
 #include "../lib/microtcp.h"
 #include "../utils/log.h"
 
+#define BUF_LEN 2048
+
 static char running = 1;
 
 static void sig_handler(int signal) {
@@ -39,23 +41,69 @@ static void sig_handler(int signal) {
 }
 
 int main(int argc, char **argv) {
-  uint16_t port;
+    uint16_t port;
+    int opt;
+    char *ipstr = NULL;
+    microtcp_sock_t socket;
+    struct sockaddr_in server_address;
+    socklen_t server_address_len;
+    int ret;
+    char buffer[BUF_LEN];
+    struct timespec start_time;
+    struct timespec end_time;
 
+    while ((opt = getopt (argc, argv, "p:a:")) != -1) {
+        switch (opt)
+        {
+            case 'p':
+                port = atoi (optarg);
+                break;
+            case 'a':
+                ipstr = strdup (optarg);
+                break;
+
+            default:
+                printf (
+                        "Usage: bandwidth_test [-s] [-m] -p port -f file"
+                                "Options:\n"
+                                "   -p <int>            The listening port of the server\n"
+                                "   -a <string>         The IP address of the server. This option is ignored if the tool runs in server mode.\n"
+                exit (EXIT_FAILURE);
+        }
+    }
   /*
    * Register a signal handler so we can terminate the client with
    * Ctrl+C
    */
-  signal(SIGINT, sig_handler);
+    signal(SIGINT, sig_handler);
 
-  LOG_INFO("Start receiving traffic from port %u", port);
-  /*TODO: Connect using microtcp_connect() */
-  while(running) {
-    /* TODO: Measure time */
-    /* TODO: Receive using microtcp_recv()*/
-    /* TODO: Measure time */
-    /* TODO: Do other stuff... */
-  }
+  /*create a microtcp socket*/
+    socket = microtcp_socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 
+    LOG_INFO("Start receiving traffic from port %u", port);
+
+    memset(&server_address, 0, sizeof(struct sockaddr));
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(port);
+    server_address.sin_addr.s_addr = ipstr;
+    server_address_len = sizeof(struct sockaddr_in);
+    ret = microtcp_connect(&socket,(struct sockaddr *) &server_address,  server_address_len);
+
+    if(ret!=0){
+        LOG_ERROR("Failed to connect");
+        return -EXIT_FAILURE;
+    }
+
+    while(running) {
+        clock_gettime(CLOCK_MONOTONIC_RAW, &start_time);
+        microtcp_recv(&socket,buffer, BUF_LEN,0);
+        clock_gettime(CLOCK_MONOTONIC_RAW, &end_time);
+        double elapsed = end_time.tv_sec - start_time.tv_sec + (end_time.tv_nsec - start_time.tv_nsec) * 1e-9;
+        printf("The time is: %d\n", elapsed);
+    }
+
+    LOG_INFO("Going to terminate microtcp connection...");
+    microtcp_shutdown(&socket, SHUT_RDWR);
   /* Ctrl+C pressed! Store properly time measurements for plotting */
 }
 
